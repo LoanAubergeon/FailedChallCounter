61d7037eb823057778681fc835ebbab97c480d06
Loan <laubergeon@gmail.com>
2020-05-04 07:40:03 +0200

    Updates

---
+++ precache-manifest.e87f3ecc54c7a89a82d576d7a822b26a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a74491d47ed5427909c067ffd140906",
+    "url": "/FailedChallCounter/index.html"
+  },
+  {
+    "revision": "c33dceb5aa8c10930842",
+    "url": "/FailedChallCounter/static/css/2.519e3fdd.chunk.css"
+  },
+  {
+    "revision": "a57b4513c1d4575461a0",
+    "url": "/FailedChallCounter/static/css/main.a4261f5e.chunk.css"
+  },
+  {
+    "revision": "c33dceb5aa8c10930842",
+    "url": "/FailedChallCounter/static/js/2.5d09b3c3.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/FailedChallCounter/static/js/2.5d09b3c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57b4513c1d4575461a0",
+    "url": "/FailedChallCounter/static/js/main.32c09713.chunk.js"
+  },
+  {
+    "revision": "a8378f247abfb16fa0ae",
+    "url": "/FailedChallCounter/static/js/runtime-main.bb410392.js"
+  }
+]);
