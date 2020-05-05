79ecfe8415ccd160207827ab4e3cfaf0a53e28cc
Loan <laubergeon@gmail.com>
2020-05-05 13:00:29 +0200

    Delete config.js

--- src/components/Config/config.js
+++
@@ -1,9 +0,0 @@
-/** Must be ignored */
-export default {
-  apiKey: "AIzaSyBNtfu70bVaj2KiA0Tlpv7rSiFxQGt_His",
-  authDomain: "chall-counter.firebaseapp.com",
-  databaseURL: "https://chall-counter.firebaseio.com",
-  projectId: "chall-counter",
-  storageBucket: "chall-counter.appspot.com",
-  messagingSenderId: "223353068983"
-}
